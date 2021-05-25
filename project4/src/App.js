b4cb34f10ea49bb902d0fb858d9b6d897363bf9a
Christian <christian.garba@gmail.com>
2021-05-24 23:10:40 -0700

    done

--- project4/src/App.js
+++ project4/src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
         <h1>Practice your React skills.</h1>
-        <p>Learn WordPress, backend languages like Laravel (PHP) or Python/Django!</p>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ project4/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Home</h3>
+                <h3>About</h3>
+                <h3>Contact</h3>
+            </div>
+        );
+    }
+}
+
+export default NavBar;
